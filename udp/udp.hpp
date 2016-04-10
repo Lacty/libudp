bad6e4a5399d13e772f9e956e3fb8c2c06b98611
Lacty <akira206@gmail.com>
2016-04-10 21:32:15 +0900

    update: cross platform

---
+++ udp/udp.hpp
@@ -0,0 +1,41 @@
+
+#pragma once
+
+#if _WINDOWS
+  #include <WinSock2.h>
+#else
+  #include <sys/socket.h>
+  #include <netinet/in.h>
+  #include <arpa/inet.h>
+#endif
+
+#include <iostream>
+#include <string>
+#include <assert.h>
+
+
+class Udp {
+private:
+#if _WINDOWS
+  WSAData     wsa_data_;
+  SOCKET      sock_;
+#else
+  int sock_;
+#endif
+
+  sockaddr_in addr_;
+  u_short     port_;
+  std::string ip_;
+
+  void createSocket();
+
+public:
+  Udp();
+  ~Udp();
+
+  void initAddr(u_short port, const std::string& ip);
+
+  std::string recieve();
+
+  void send(const std::string& data);
+};
