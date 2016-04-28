ff3f3bff93e5a0d11bc2ace061146252310b7981
Lacty <akira206@gmail.com>
2016-04-28 21:56:42 +0900

    update

--- udp/udp.cpp
+++ udp/udp.cpp
@@ -3,15 +3,35 @@
 
 
 Udp::Udp() {
-#if _WINDOWS
-  int err = WSAStartup(MAKEWORD(2, 0), &wsa_data_);
-  assert(err == 0);
+#if defined(WIN32) || defined(_WINDOWS)
+  if (WSAStartup(MAKEWORD(2, 0), &wsa_)) assert(!"error: WSAStart");
+  // reset sock var
+  sock_ = 0;
+  // clean addr array
+  memset(&addr_, 0, sizeof(addr_));
+#else
+  // reset sock var
+  sock_ = 0;
+  // clean addr array
+  memset(&addr_, 0, sizeof(addr_));
 #endif
 }
 
 Udp::~Udp() {
-#if _WINDOWS
+  closeSock();
   WSACleanup();
+}
+
+
+void Udp::createSock() {
+  closeSock();
+
+  sock_ = ::socket(AF_INET, SOCK_DGRAM, 0);
+  assert(sock_ != 0);
+}
+
+void Udp::closeSock() {
+#if defined(WIN32) || defined(_WINDOWS)
   closesocket(sock_);
 #else
   close(sock_);
@@ -19,68 +39,61 @@ Udp::~Udp() {
 }
 
 
-void Udp::createSocket() {
-  sock_ = socket(AF_INET, SOCK_DGRAM, 0);
-
-  // err check
-  if (sock_ < 0) {
-    std::cout << "[CreateSocket] socket failed" << std::endl;
-    perror("CreateSocket");
-  }
-}
-
-void Udp::initAddr(u_short port, const std::string& ip) {
-  createSocket();
-
-  port_ = port;
-  ip_ = ip;
-
-  bool is_host = ip_.empty();
+void Udp::init(const int port, const std::string& ip) {
+  createSock();
 
   addr_.sin_family = AF_INET;
-  addr_.sin_port = htons(port_);
-  
-  addr_.sin_addr.s_addr
-    = is_host
-    ? INADDR_ANY
-    : inet_addr(ip_.c_str());
+  addr_.sin_port = htons(port);
+  addr_.sin_addr.s_addr = inet_addr(ip.c_str());
+}
 
-  // named the socket(bind)
-  if (is_host) {
-    int res = bind(sock_, (sockaddr*)&addr_, sizeof(addr_));
-    if (res < 0) {
-      std::cout << "err bind" << std::endl;
-    }
-  }
+void Udp::init(const int port) {
+  createSock();
+
+  addr_.sin_family = AF_INET;
+  addr_.sin_port = htons(port);
+  addr_.sin_addr.s_addr = INADDR_ANY;
+
+  int res = ::bind(sock_, (sockaddr*)&addr_, sizeof(addr_));
+
+  // err check if Bind Failed
+  assert(res == 0);
 }
 
 
-std::string Udp::recieve() {
-  std::string data;
-  data.clear();
-
+void Udp::recv(std::string& data) {
   char buf[BUF_SIZE + 1];
+  memset(buf, 0, sizeof(buf));
 
-  int nrecv = recv(sock_, buf, BUF_SIZE, 0);
+  int nrecv = ::recv(sock_, buf, BUF_SIZE, 0);
+
+  std::cout << buf << " : " << nrecv << std::endl;
 
   data = buf;
   data.resize(nrecv);
 
-  // err check
-  if (nrecv <= 0) {
-    std::cout << "[Recv] Recv Failed:" << nrecv << std::endl;
-    perror("Recv");
-  }
-
-  return std::move(data);
+  // err check if Recv Failed
+  assert (nrecv != 0);
 }
 
 void Udp::send(const std::string& data) {
-  int nwrite = sendto(sock_, data.c_str(), data.length(), 0, (sockaddr*)&addr_, sizeof(addr_));
+  int nwrite = ::sendto(sock_, data.c_str(), data.length(), 0, (sockaddr*)&addr_, sizeof(addr_));
 
-  // err check
-  if (nwrite <= 0) {
-    std::cout << "[SendTo] SendTo Failed:" << nwrite << std::endl;
-    perror("SendTo");
-  }
+  // err check if Sendto Failed
+  assert(nwrite != 0);
+}
+
+std::string Udp::recv() {
+  std::string data;
+  this->recv(data);
+  return std::move(data);
+}
+
+// operators
+void Udp::operator >> (std::string& data) {
+  recv(data);
+}
+
+void Udp::operator << (const std::string& data) {
+  send(std::move(data));
 }
